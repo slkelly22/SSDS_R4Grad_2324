eb5f6cc340e455b1456838293dd95fe28cc72c5d
slkelly22 <slkelly@go.olemiss.edu>
2023-07-11 12:53:29 -0500

    Week 1 code, basics

---
+++ R4Grad_Week1.R
@@ -0,0 +1,22 @@
+#R for Graduate Students, 2023-24
+#Week 1
+#S.Kelly
+
+#shows you downloaded packages
+library()
+
+#how to download a package, put package name in parentheses; alternatively, use Packages - Install button
+intall.packages("tidyverse")
+
+#shows you active packages
+search()
+
+#loads the tidyverse package so you can use it
+library(tidyverse)
+#note: you can't load/activate a package that you haven't installed. 
+#You only need to install a package one, but you need to load a package every time you want to use the package. 
+
+#if you want help, you can use ? 
+?mean
+
+#assignment operator is <-
