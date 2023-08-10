66091c5c088a8e6c1b3d3d3a8da5be1bf630addc
slkelly22 <slkelly@go.olemiss.edu>
2023-08-10 16:30:05 -0500

    Adding info @ directory, tidy data, assignment operator, naming conventions

--- R4Grad_Week1.R
+++ R4Grad_Week1.R
@@ -2,7 +2,48 @@
 #Week 1
 #S.Kelly
 
-#shows you downloaded packages
+#getting oriented, setting preferences, understanding panels
+#setting up your directory
+getwd()
+setwd()
+#always know where you are in your files? saving your files vs. saving your workspace
+#what is a project in R?
+
+#getting to know Base R
+#R is case sensitive
+#most of the mistakes you'll make in R are spelling errors [or not loading libraries]
+
+#What is Tidy Data? 
+
+#Building a dataset / dataframe within R
+
+#the assignment operator
+
+#naming conventions
+01var <- c(3, 2, 3, 4, 7) #that doesn't work
+var01 <- c(3, 2, 3, 4, 7) #that works
+#var name is too too non-descript, what would be better? 
+tea_ratings <- c(3, 2, 3, 4, 7)
+#you can take an assignment and do things with it
+mean(tea_ratings)
+tea_ratings *5
+
+tea_ratings <= 3 #returns a logical vector
+tea_ratings !=3
+
+#you can remove objects from your environment
+rm(var01)
+#Be careful: you can easily write over objects and entire dataframes if you use the same name
+
+#what is you have categorical data? 
+my_fav_colors <- c("olive green", "mustard yellow", "dark grey")
+print(my_fav_colors)
+mean(my_fav_colors) #doesn't work; why? 
+
+#selecting in Base R with position [] or save to later session?
+
+#Understanding Packages
+#shows your downloaded packages
 library()
 
 #how to download a package, put package name in parentheses; alternatively, use Packages - Install button
