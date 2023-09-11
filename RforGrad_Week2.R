9c93b9c4179e3ef609515bdd178139e0285134d0
slkelly22 <slkelly@go.olemiss.edu>
2023-09-11 15:47:18 -0500

    adding activities on changing data types and installing lubridate and changing dates

--- RforGrad_Week2.R
+++ RforGrad_Week2.R
@@ -30,28 +30,47 @@ library(readxl)
 #Reading in data
 #with Base R
 squirrels_data <- read.csv("~/Desktop/Abbrev_Central_Park_Squirrels_2018.csv")
+
 #Alternative: reading in with readr and then grabbing code
 
 View(squirrels_data)
 nrow(squirrels_data)
 ncol(squirrels_data)
 dim(squirrels_data)
-summary(squirrels_data)
 
 head(squirrels_data)
 tail(squirrels_data, 15)
 
+#base R
 summary(squirrels_data)
-
+#what's the structure of the date? 
 str(squirrels_data$date)
+class(squirrels_data$date)
+
+#activity: what's the structure and class of tail flags? 
+str(squirrels_data$tail_flags)
+class(squirrels_data$tail_flags)
+
+#First Tidy (dplyr) function: count
+#when were more squirrels observed in this dataset? 
+squirrels_data %>%
+  count(shift)
+
+#what ages are represented in the dataset?
+squirrels_data %>%
+  count(age)
+
 
 #you can always install packages as you work (although it's best to load everything in the beginning)
-?lubridate
-search()
+#lubridate is part of the tidyverse ecosystem but it's not installed with main tidyverse package so you have to install it separately
+install.packages("lubridate")
 library(lubridate)
 search()
+View(squirrels_data)
+class(squirrels_data$date)
 squirrels_data$date <- mdy(squirrels_data$date)
 View(squirrels_data)
+class(squirrels_data$date)
 
 summary(squirrels_data)
 
@@ -61,6 +80,16 @@ str(squirrels_data$running)
 #let's change that to numeric
 squirrels_data$running <-as.numeric(squirrels_data$running)
 summary(squirrels_data)
-
+View(squirrels_data)
 squirrels_data %>%
   count(running)
+
+class(squirrels_data$running)
+squirrels_data$running <-as.logical(squirrels_data$running)
+class(squirrels_data$running)
+View(squirrels_data)
+
+#how do we export this dataset (the one with the corrected date)
+#1. What did you save it as? Same object or new? 
+#Base R
+write.csv(squirrels_data, "Squirrels_Data_with_Date.csv")
