9b620589722973f73652d34e6d3f0fc6a4aefa77
slkelly22 <slkelly@go.olemiss.edu>
2023-09-11 10:16:11 -0500

    deleted and moved content from week 1 to week 2 because we didn't have time to cover in week 1

--- R4Grad_Week1.R
+++ R4Grad_Week1.R
@@ -64,24 +64,4 @@ tea_dataset <- data.frame(tea_type, steep_time, tea_ratings)
 View(tea_dataset)
 str(tea_dataset)
 
-#Understanding Packages
-#shows your downloaded packages
-library()
-
-#how to download a package, put package name in parentheses; alternatively, use Packages - Install button
-intall.packages("tidyverse")
-
-#shows you active (loaded) packages
-search()
-
-#loads the tidyverse package so you can use it
-library(tidyverse)
-#note: you can't load/activate a package that you haven't installed. 
-#You only need to install a package one, but you need to load a package every time you want to use the package. 
-
-#if you want help, you can use ? 
-?mean
-
-#Next Week
-#reading in data
-SK_geology_7.10.23 <- read.csv("~/Desktop/SK_geology_7.10.23.csv")
+#moved the content beneath this (in previous iteration) to Week 2 because we didn't cover in Week 1
