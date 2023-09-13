f86de6e5b02e3d2437fbc75ec9e9bfbfa000650e
slkelly22 <slkelly@go.olemiss.edu>
2023-09-13 16:49:28 -0500

    added a note for next steps

--- RforGrad_Week3.R
+++ RforGrad_Week3.R
@@ -2,7 +2,6 @@
 #Week 3
 #S.Kelly
 
-
 library(tidyverse)
 
 squirrels_data <- read.csv("~/Desktop/Abbrev_Central_Park_Squirrels_2018.csv")
@@ -11,3 +10,5 @@ squirrels_data <- read.csv("~/Desktop/Abbrev_Central_Park_Squirrels_2018.csv")
 #follow up on a question from Week 2 about whether you can count more than one variable at a time; answer: yes
 squirrels_data %>%
   count(shift, age) 
+
+#also should answer the lubridate / date --> back to number issue from last week
